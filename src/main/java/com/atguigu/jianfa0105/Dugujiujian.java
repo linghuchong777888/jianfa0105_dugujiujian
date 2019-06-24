e51d3fa63713bad298339e9c9aa7299ef7cddc30
linghuchong <linghuchong777888@126.com>
2019-06-24 14:43:07 +0800

    huashan jianfa

--- src/main/java/com/atguigu/jianfa0105/Dugujiujian.java
+++ src/main/java/com/atguigu/jianfa0105/Dugujiujian.java
@@ -2,6 +2,7 @@ package com.atguigu.jianfa0105;
 
 public class Dugujiujian {
     public static void main(String[] args) {
-        System.out.println("hello dugujiujian!!!!");
+        System.out.println("hello dugujiujian123123!!!!");
+
     }
 }
---
+++ src/main/java/com/atguigu/jianfa0105/Huashanjianfa.java
@@ -0,0 +1,7 @@
+package com.atguigu.jianfa0105;
+
+public class Huashanjianfa {
+    public static void main(String[] args) {
+        System.out.println("huashan");
+    }
+}
